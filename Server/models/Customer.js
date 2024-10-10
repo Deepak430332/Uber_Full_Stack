6f3b35fd38ed2655903edcf2a39711e79015261c
Deepak430332 <sangeetham.deepak@gramener.com>
2024-10-10 13:38:44 +0530

    second commit

---
+++ Server/models/Customer.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const CustomerSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    gender: { type: String, required: true },
+    passengers_count: { type: Number, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    pickup_location: { type: String, required: true },
+    drop_location: { type: String, required: true },
+    vehicle_type: { type: String, required: true },
+    trip_distance:{type:Number,required: true},
+    trip_duration:{type:Number,required:true}
+});
+
+module.exports = mongoose.model('Customer', CustomerSchema);
